532ff4d18bcf5d7c5a96090ec588f90a9b6d73d4
Z <pearsozj@dukes.jmu.edu>
2021-10-13 19:29:41 -0700

    Create Seconds to Minutes Conversion.py

---
+++ Seconds to Minutes Conversion.py
@@ -0,0 +1,10 @@
+#This is a code meant to take an input of seconds and convert it to minutes
+
+seconds = 120
+
+Minutes = seconds// 60
+
+Remaining_seconds = seconds % 60
+print(seconds, "seconds is")
+
+print (Minutes, "minutes and", Remaining_seconds, "seconds")
